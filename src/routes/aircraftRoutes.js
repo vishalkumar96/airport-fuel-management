a58ba77da1451acc44bda4142efef85ef58fe7a1
vishalkumar96 <vkumar@techferry.com>
2020-09-15 10:54:26 +0530

    payload validate

--- src/routes/aircraftRoutes.js
+++ src/routes/aircraftRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const aircraftConroller = require('../controller/aircraftController');
 const {tokenVerification} = require('../auth/tokenVerification');
 const payloadValidate = require('../utils/payloadValidate')
-const validate = require('../validation/validate');
+const validate = require('../utils/validate');
 
 
 router.route('/addAircraft').post(tokenVerification, validate(payloadValidate.addAircraft), aircraftConroller.addAircraft)
--- src/routes/airportRoutes.js
+++ src/routes/airportRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const airportConroller = require('../controller/airportConroller');
 const {tokenVerification} = require('../auth/tokenVerification');
-const validate = require('../validation/validate');
+const validate = require('../utils/validate');
 const payloadValidate = require('../utils/payloadValidate')
 
 router.route('/addAirport').post(tokenVerification,validate(payloadValidate.addAirport), airportConroller.addAirport)
